a22d7915093efb098d2af113f5f27d4c404f6d7a
estefanyrizo <estefanyrizo@icloud.com>
2022-10-16 22:59:29 -0600

    Solucionando errores

--- app/Database/Seeds/Usuario.php
+++ app/Database/Seeds/Usuario.php
@@ -14,13 +14,19 @@ class Usuario extends Seeder
         $apellido = "Rizo";
         $telefono = "+50583621547";
         $biografia = "Me gusta el pan";
+        $correo = "admin@gmail.com";
+        $isAdmin = 1;
+        $foto_perfil = "https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg";
         $data = [
-            'usuario' => $usuario,
-            'clave'    => $clave,
+            'username' => $usuario,
+            'hash'    => $clave,
             'nombre'    => $nombre,
             'apellido'  => $apellido,
             'telefono'  => $telefono,
             'biografia' => $biografia, 
+            'correo' => $correo,
+            'isAdmin' => $isAdmin,
+            'foto_perfil' => $foto_perfil
         ];
 
         // Using Query Builder
